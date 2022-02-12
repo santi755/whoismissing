782410987616454b65ddf598bd9c726a90036577
Sergio <sergioalmela@gmail.com>
2022-02-12 11:15:54 +0100

    Get connected users to voice channel

--- src/bot.js
+++ src/bot.js
@@ -4,38 +4,48 @@ import { GetUsersFromFile } from './services/manageFile.js'
 
 dotenv.config();
 
-const usersFromFile = GetUsersFromFile()
+// const usersFromFile = GetUsersFromFile()
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+    ],
+});
+
+let channelID = process.env.DISCORDJS_CHANNEL_ID
 
 client.on('ready', () => {
     console.log(`${client.user.username} is Connected!`)
 })
 
-client.on('messageCreate', async message => {
+client.on('messageCreate', message => {
     if (message.content === '/whoismissing') {
-        console.log("PEOPLE => ", usersFromFile)
+
+        // List of connected users to channel voice
+        let users = getConnectedUsers(message)
+
         const embed = new MessageEmbed()
         .setColor('#ae2c72')
         .setTitle('Who is missing in Daily?')
         .setDescription('- Some description here\n- Second line');
 
         message.reply({ ephemeral: true, embeds: [embed] });
-/*
-        message.reply('Loading who is missing...')
-        
-        let channel = client.channels.cache.get("237324836333027329")
-        console.log(`Channel is ${channel}`)
-
-        channel.members.forEach(member => {
-            let userData = client.users.fetch(member).then(data => {
-                console.log("userData.username => ", data.username)
-            })
-            
-        }) 
-*/
     }
 })
+
+function getConnectedUsers(message) {
+    let users = [];
+    message.guild.channels.cache.get(channelID).members.forEach((member) => {
+        // member.send('R!');
+        users.push(member.id);
+    });
+
+    return users;
+}
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
